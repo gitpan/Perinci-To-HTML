daf170c30dfa7d803c39efc11d221d43b6191cf5
Steven Haryanto <stevenharyanto@gmail.com>
2012-03-16 01:57:21 +0000

    Import of SHARYANTO/Perinci-To-HTML-0.01 from CPAN.
    gitpan-cpan-distribution: Perinci-To-HTML
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Perinci-To-HTML-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Perinci/To/HTML/I18N.pm
@@ -0,0 +1,33 @@
+package Perinci::To::HTML::I18N;
+
+use parent 'Perinci::To::PackageBase::I18N';
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Project class for Perinci::To::HTML
+
+__END__
+=pod
+
+=head1 NAME
+
+Perinci::To::HTML::I18N - Project class for Perinci::To::HTML
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Perinci/To/HTML/I18N/id.pm
@@ -0,0 +1,43 @@
+package Perinci::To::HTML::I18N::id;
+use parent qw(Perinci::To::HTML::I18N Perinci::To::PackageBase::I18N::id);
+
+use Locale::Maketext::Lexicon::Gettext;
+our %Lexicon = %{ Locale::Maketext::Lexicon::Gettext->parse(<DATA>) };
+
+our $VERSION = '0.01'; # VERSION
+
+#use Data::Dump; dd \%Lexicon;
+
+1;
+# ABSTRACT: Indonesian translation for Perinci::To::HTML
+
+
+=pod
+
+=head1 NAME
+
+Perinci::To::HTML::I18N::id - Indonesian translation for Perinci::To::HTML
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__DATA__
+
+msgid  "Parameters"
+msgstr "Parameter"
+
